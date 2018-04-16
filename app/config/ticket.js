c4335d555ea0ccba9513e05e31835ca7c5a61b62
ranapat <ranapat@gmail.com>
2018-04-16 07:28:21 +0200

    change scratch ticket background

--- app/config/ticket.js
+++ app/config/ticket.js
@@ -2,7 +2,7 @@ export default {
   horizontal: 3,
   vertical: 3,
   spacing: 5,
-  background: 0x505050,
+  background: 0xd05d2d,
   revealAllAfter: 0.6,
   revealSingleAfter: 0.6,
   keepSingleBelow: 0.25,
